7f28d6599fbf3083e4fe6bec61bf6bf328b04a91
Marty Powell <jpowel25@gmail.com>
2017-08-01 09:34:46 -0400

    refactored

--- app/members/members.js
+++ app/members/members.js
@@ -10,111 +10,50 @@
       });
     }])
 
-    .factory('knowledgeService', ['$http', knowledgeService])
+    .controller('MembersCtrl', ['$scope', '$window', '$log', 'membersService', 'knowledgeService',
+      function ($scope, $window, $log, membersService, knowledgeService) {
 
-    .controller('MembersCtrl', ['$scope', '$window', 'membersService', 'knowledgeService', function ($scope, $window, membersService, knowledgeService) {
-      var self = this;
-      self.members = [];
+        var self = this;
+        self.members = [];
+        self.knowledgeSelection = "";
+        self.activeKnowledgeItem = "";
 
-      self.members = membersService.Get();
-      self.filteredMembers = self.members;
+        self.clearFilter = function () {
+          self.filterText = "";
+          self.activeKnowledgeItem = {};
+        };
 
-      /* membersService.Get().then(function (members) {
-        self.members = self.members.concat(members);
-        
-        $scope.$digest();
-      }); */
+        /**
+         * Get Members 
+         */
 
-      knowledgeService.Get().then(function (knowledgeList) {
-        self.knowledgeList = knowledgeList.sort(function (itemA, itemB) {
-          return itemB.count - itemA.count;
-        });
-        $scope.$digest();
-      });
+        membersService.Get()
+          .then(getMembersSuccess)
+          .catch(errorCallback);
 
-      self.filterKnowledge = filterKnowledge;
-      self.knowledgeSelection = "";
-      self.activeKnowledgeItem = "";
 
-      self.clearFilter = function() { 
-        self.filterText = "";
-        self.activeKnowledgeItem = {};
-      };
+        function getMembersSuccess(members) {
+          self.members = members;
+          self.filteredMembers = members;
+        }
 
-      function filterKnowledge(item) {
-        //Set the active item
-        self.activeKnowledgeItem = item;
+        /**
+         * Get Knowledge List
+         */
 
-        self.filteredMembers = self.members.filter(function(member) {
-          var isMatch = false;
-          var typeArr;
-            for (var key in member) {
-              typeArr = member[key];
-              if (Array.isArray(typeArr)) {
-                var valArr = typeArr.map(function (item) {
-                  return item.toUpperCase();
-                });
+        knowledgeService.Get()
+          .then(getKnowledgeSuccess)
+          .catch(errorCallback);
 
-                var matches = valArr.filter(function(val) {
-                  return val === item.value.toUpperCase();
-                });
-
-                if (matches.length) {
-                  isMatch = true;
-                }
-              }
-            }
-            return isMatch;
-        });
-      }
-    }]);
-
-  function knowledgeService($http) {
-    function get() {
-      return new Promise(function (resolve, reject) {
-        $http({
-          method: 'GET',
-          url: '/data/guild-members.json',
-          cache: true
-        }).then(function successCallback(response) {
-          var members = response.data || [];
-          var knowledgeList = [];
-          var typeArr;
-
-          members.forEach(function (member) {
-            for (var key in member) {
-              typeArr = member[key];
-              if (Array.isArray(typeArr)) {
-
-                typeArr.forEach(function (type) {
-                  let existingItem = knowledgeList.filter(function (item) {
-                    return item.value === type;
-                  });
-
-                  if (existingItem.length) {
-                    existingItem[0].count += 1;
-                  } else {
-                    knowledgeList.push({
-                      type: key,
-                      value: type,
-                      count: 1
-                    });
-                  }
-                });
-              }
-            }
+        function getKnowledgeSuccess(knowledgeList) {
+          self.knowledgeList = knowledgeList.sort(function (itemA, itemB) {
+            return itemB.count - itemA.count;
           });
+        }
 
-          resolve(knowledgeList);
-        }, function (response) {
-          console.error('something went wrong retreiving knowledge');
-          reject(response);
-        });
-      });
-    }
-
-    return {
-      Get: get
-    };
-  }
+        function errorCallback(errorMsg) {
+          $log.error("Error: " + errorMsg);
+        } 
+        
+      }]);
 })(angular.module('developmentGuild'));
