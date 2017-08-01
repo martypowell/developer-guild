e4de9d8b2f1a252a92e0d36a44bd0f5c4c411f02
Marty Powell <jpowel25@gmail.com>
2017-08-01 09:34:38 -0400

    fixed member service

--- app/members/members-service.js
+++ app/members/members-service.js
@@ -1,46 +1,46 @@
 (function (app) {
-    'use strict';
+  'use strict';
 
-    app.factory('membersService', ['$http', '$q', memberService]);
+  app.factory('membersService', ['$http', '$q', memberService]);
 
-    function memberService($http, $q) {
-        return {
-            Get: get
-        };
+  function memberService($http, $q) {
+    return {
+      Get: get
+    };
 
-        function get() {
-            $http.get('/data/guild-members.json', {
-                    transformResponse: sortMemberData
-                })
-                .then(sendMemberResponseData)
-                .catch(sendMemberServiceError);
-        }
-
-        function sortMemberData(response, headerGetter) {
-            var members = angular.fromJson(response);
-            return members.sort(function (a, b) {
-                var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-                var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-                if (nameA < nameB) {
-                    return -1;
-                }
-                if (nameA > nameB) {
-                    return 1;
-                }
-
-                // names must be equal
-                return 0;
-            });
-        }
-
-        function sendMemberResponseData(response) {
-            return response.data;
-        }
-
-        function sendMemberServiceError(response) {
-            return $q.reject('Error: ' + response.status);
-        }
-
+    function get() {
+      return $http.get('/data/guild-members.json', {
+        transformResponse: sortMemberData
+      })
+        .then(sendMemberResponseData)
+        .catch(sendMemberServiceError);
     }
+
+    function sortMemberData(response, headerGetter) {
+      var members = angular.fromJson(response);
+      return members.sort(function (a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+
+        // names must be equal
+        return 0;
+      });
+    }
+
+    function sendMemberResponseData(response) {
+      return response.data;
+    }
+
+    function sendMemberServiceError(response) {
+      return $q.reject('Error: ' + response.status);
+    }
+
+  }
 
 })(angular.module('developmentGuild'));
